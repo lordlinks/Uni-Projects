9042c3fadacae7707ec858bbe90eb6dfb9bf3ba5
Douglas Green <douglas.green@uqconnect.edu.au>
2015-10-02 13:29:29 +1000

    compy swap

---
+++ Book/Book/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book
+{
+    public class Books
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Book/Book/Controllers/textfileController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Book.Controllers
+{
+    public class textfileController : ApiController
+    {
+        List<Books> books = new List<Books>
+        {
+            //product inserts here
+        };
+
+        public IHttpActionResult GetAllBooks()
+        {
+            return Ok(books);
+        }
+
+        public IHttpActionResult GetBook(string id)
+        {
+            var book = books.FirstOrDefault((x) => x.Id == id);
+            if (book == null)
+            {
+                //do something with nothing values
+            }
+            return Ok(books);
+        }
+
+        // GET: api/textfile
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/textfile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/textfile
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/textfile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/textfile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
