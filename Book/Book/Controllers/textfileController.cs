6e5e756fe93324df0b2a7bfa8e5df9ed06593452
Douglas Green <douglas.green@uqconnect.edu.au>
2015-10-05 14:00:02 +1000

    update to the controller i think this will work.

--- Book/Book/Controllers/textfileController.cs
+++ Book/Book/Controllers/textfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,7 +12,12 @@ namespace Book.Controllers
     {
         List<Books> books = new List<Books>
         {
-            //product inserts here
+            new Books {Id="0470376848", Name="Service Oriented Architecture (SOA) For Dummies", Author="Judith Hurwitz", Year=2009, Price=25.27F,Stock=3},
+            new Books {Id="0470476248", Name="Service-Oriented Architecture: Concepts Technology and Design", Author="Thomas Erl", Year=2005, Price=46.36F,Stock=2},
+            new Books {Id="0485965215", Name="Service-Oriented Architecture: A Field Guide to Integrating XML and Web Services", Author="Thomas Erl", Year=2007, Price=43.16F,Stock=1},
+            new Books {Id="0456235975", Name="SOA in Practice: The Art of Distributed System Design", Author="Nicolai M. Josuttis", Year=2007, Price=24.59F,Stock=1},
+            new Books {Id="0487965321", Name="SOA Made Simple", Author="Lonneke Dikmans", Year=2012, Price=13.72F,Stock=2},
+            new Books {Id="0412359687", Name="SOA with REST: Principles Patterns & Constraints for Building Enterprise Solutions with REST", Author="Thomas Erl", Year=2012, Price=48.25F,Stock=3}
         };
 
         public IHttpActionResult GetAllBooks()
